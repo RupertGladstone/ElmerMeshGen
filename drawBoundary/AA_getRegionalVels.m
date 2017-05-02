a94d26e99796f3f1036e37208214a5597bc2f098
elmeruser <rupertgladstone1972@gmail.com>
2017-05-02 14:21:04 +0200

    allow mix of auto ice-ocean boundary (from bedmap2 masks) and user defined boundary

--- drawBoundary/AA_getRegionalVels.m
+++ drawBoundary/AA_getRegionalVels.m
@@ -1,5 +1,6 @@
 
-clf; clear; params;
+clf; clear; 
+params;
 
 ncid = netcdf.open(ncfile,'NOWRITE');
 varid = netcdf.inqVarID(ncid,xvelName);
@@ -19,7 +20,7 @@ if and(and(exist('i_l'), exist('i_r')), and(exist('j_b'), exist('j_t')))
 else
     fprintf(['Drag mouse to select rectangle covering domain of ' ...
              'interest. \n'])
-    rect = getrect();
+    rect = getrect()
     j_b  = floor(rect(2));
     j_t  = j_b + floor(rect(4));
     i_l  = floor(rect(1));
@@ -49,24 +50,30 @@ else
 end
 
 
-if WholeIceSheet
+if DrawBothBoundaries
+    fprintf(['Click points to define mesh ice-ocean boundary (double click ' ...
+             'to end) \n'])
+    [ob_x, ob_y] = getpts();
+end
+if (DrawBothBoundaries|DrawInlandBoundary)
+    fprintf(['Click points to define mesh interior ice boundary (start ' ...
+             'near end of previous points, finish near start of previous ' ...
+             'points, double click to end) \n'])
+    [ib_x, ib_y] = getpts();
+end
+if (WholeIceSheet|DrawInlandBoundary)
     fprintf(['Calculating contour to use as ice-ocean boundary \n'])
     [cc, hh] = contour(vnorm,[0.01,0.01]);
     numPoints = cc(2,1);
     mainContour = cc(:,2:numPoints+1);
     clear cc, hh;
-    h = plot(mainContour(1,:), mainContour(2, :), 'k');
+    %    h = plot(mainContour(1,:), mainContour(2, :), 'k');
     ob_x = mainContour(1,:);
     ob_y = mainContour(2,:);
+    ob_x = ob_x';ob_y = ob_y';
+end
+if (WholeIceSheet)
     ib_x = 0; ib_y = 0;
-else
-    fprintf(['Click points to define mesh ice-ocean boundary (double click ' ...
-             'to end) \n'])
-    [ob_x, ob_y] = getpts();
-    fprintf(['Click points to define mesh interior ice boundary (start ' ...
-             'near end of previous points, finish near start of previous ' ...
-             'points, double click to end) \n'])
-    [ib_x, ib_y] = getpts();
 end
 
 fprintf(['Writing boundaries to .geo file for gmsh. \n']);
@@ -74,10 +81,32 @@ fprintf(['Writing boundaries to .geo file for gmsh. \n']);
 ob_x = xmin+ob_x*dx; ob_y = ymin+ob_y*dx;
 ib_x = xmin+ib_x*dx; ib_y = ymin+ib_y*dx;
 
-ib_nx = length(ib_x);ob_nx = length(ob_x);id=1;firstId = id;
+if (DrawInlandBoundary)
+    ib = [ib_x,ib_y];
+    ob = [ob_x,ob_y];         
+    % Find the closest points from the whole Antarctic ocean
+    % boundary (ice front) to the start and end points of the
+    % inland boundary.
+    % We want the ocean boundary point closest to the end point of
+    % the inland boundary to be the first one in the ocean boundary
+    % array.  Therefore we flip along this dimension if needed. 
+    ind_end = dsearchn(ob,ib(end,:));
+    ind_st  = dsearchn(ob,ib(1,:));
+    if (ind_end>ind_st)
+        ob_use = ob(ind_st:ind_end,:);
+        ob_use = flip(ob_use,1);
+    else
+        ob_use = ob(ind_end:ind_st,:);
+    end
+    ob_x = ob_use(:,1);
+    ob_y = ob_use(:,2);
+end
+
+ib_nx = length(ib_x);ob_nx = length(ob_x);
+id=1;firstId = id;
 
 fid=fopen(boundaryFileOut,'w');
-fprintf(fid,['lc=100000;\n']);
+fprintf(fid,['lc=1000;\n']);
 SplineLineOB = 'Spline(1)={';
 SplineLineIB = 'Spline(2)={';
 for ii=1:ob_nx(1);
@@ -97,11 +126,6 @@ else
     for ii=1:ib_nx(1);
         fprintf(fid,'Point(%i)={%12.5f,%12.5f,0.0,lc}; \n',id,ib_x(ii),ib_y(ii));
         SplineLineIB = [SplineLineIB int2str(id) ','];
-        %    if (ii<ib_nx(1))
-        %    SplineLineIB = [SplineLineIB int2str(id) ','];
-        %else
-        %    SplineLineIB = [SplineLineIB int2str(id) '}; \n'];
-        %end
         id = id+1;
     end
     SplineLineIB = [SplineLineIB int2str(firstId) '};\n'];
@@ -131,7 +155,7 @@ fprintf(fid,'DEM_xmin=%13.5f \n',xmin+i_l*dx);
 fprintf(fid,'DEM_ymin=%13.5f \n',ymin+j_b*dx);
 fprintf(fid,'DEM_dx=%10.3f \n',dx);
 fprintf(fid,'DEM_dy=%10.3f \n',dx);
-fprintf(fid,'DEM_noval=-9999999');
+fprintf(fid,'DEM_noval=-9999999 \n');
 if (exist('velFileOut'))
     fprintf(fid,['VelDem=./' velFileOut]);
 end
--- drawBoundary/params.m
+++ drawBoundary/params.m
@@ -12,42 +12,62 @@ Secant_latitude =     -71 ;
 %% TODO: remove nx and ny from here: need to get them from array
 %% size in main script
 
-ncfile = 'antarctica_ice_velocity_900m.nc'; %% 900m resolution MEaSUREs data
+%ncfile = 'antarctica_ice_velocity_900m.nc'; %% 900m resolution MEaSUREs data
+ncfile = ['/mnt/hgfs/VMshare/drawBoundaryData/antarctica_ice_velocity_900m.nc'];
 nx = 6223  ; ny = 6223  ; dx = 900.0 ;
 %ncfile = 'antarctica_ice_velocity_450m.nc';
 %nx = 12445 ; ny = 12445 ; dx = 450.0 ;
 
 xvelName = 'vx'; yvelName = 'vy';
 
+%% whole domain (valid for either resolution)
+WholeIceSheet = false(1);
+%WholeIceSheet = true(1);
+
+%% if wholeIceSheet is false the user should choose to either 
+%% draw both the ice front and inland boundaries...
+DrawBothBoundaries = false(1);
+
+%% ...or just draw the inland boundary and let a section of the
+%% whole domain boundary be used for the ice front.
+DrawInlandBoundary = true(1);
+
+
 %% i_ and j_ are x and y coord indices at the left and right or top
 %% and bottom of the domain of interest.  Comment these out to
 %% enable interactive selection of domain of interest.
 
-%% whole domain (valid for either resolution)
-%WholeIceSheet = false(1);
-WholeIceSheet = true(1);
-i_l = 1 ; i_r = nx ;
-j_b = 1 ; j_t = ny ;
-
 %% Totten/Law Dome ish region (900m)
 %i_l = 5000 ; i_r = 6000 ; % left and right in x direction
 %j_b = 1600 ; j_t = 3000 ; % bottom and top in y direction
+%% Thwaites/PIG ish region
+%i_l = 1000 ; i_r = 2000 ; % left and right in x direction
+%j_b = 2400 ; j_t = 3350 ; % bottom and top in y direction
+%% Thwaites near GL
+%i_l = 1320 ; i_r = 1580 ; % left and right in x direction
+%j_b = 2530 ; j_t = 2710 ; % bottom and top in y direction
 %% Note, if all 4 corners (above) are defined then the script will
-%% zoom straight in to the subregion.  Otherwise the scrip will ask
+%% zoom straight in to the subregion.  Otherwise the script will ask
 %% the user to interactively define the subregion.
 
 %% name of ascii file to contain velocities for chosen subregion
 %% (comment this out to suppress writing vels to ascii)
 %velFileOut = 'AAvel900_Tottenish.asc';
-velFileOut = 'AAvel900.asc';
+%velFileOut = 'AAvel900.asc';
+%velFileOut = 'AAvel900_ThwaitesPIG.asc';
+%velFileOut = 'AAvel900_ThwaitesHR.asc';
+velFileOut = 'AAvel900_AuroraWilkes.asc';
 
 %% name of the ascii file to newritten in gmsh format (.geo)
 %% containing the boundary points defined interactively by the
 %% user. 
 %boundaryFileOut = 'TottenBoundary.geo';
-boundaryFileOut = 'AABoundary.geo';
+%boundaryFileOut = 'AABoundary.geo';
+%boundaryFileOut = 'ThwaitesPIGBoundary.geo';
+%boundaryFileOut = 'ThwaitesHRBoundary.geo';
+boundaryFileOut = 'AurWilkesBoundary.geo';
 
-%% name of the ascii file inwhich to write some yams input
+%% name of the ascii file in which to write some yams input
 %% information pertaining to the grid on which the velocity data
 %% are stored.
 yamsInputFile = 'yamsInput.asc';
